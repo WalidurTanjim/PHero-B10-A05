6b0b6a4991dee3c5eae498f9f53ec66be7731f72
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-09-23 16:53:46 +0600

    path

--- js/blog.js
+++ js/blog.js
@@ -2,5 +2,5 @@ document.getElementById('blog-btn').addEventListener('click', function(event){
     event.preventDefault();
 
     document.getElementById('toggle-btn').classList.add('hidden');
-    window.location = 'https://walidurtanjim.github.io/PHero-B10-A05/blog.html'
+    window.location = '/blog.html'
 })
