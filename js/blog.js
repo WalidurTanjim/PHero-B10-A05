5f5be3eeb5a27e4f5f43a2c626c08b3b46c4c557
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-09-23 16:39:28 +0600

    location

--- js/blog.js
+++ js/blog.js
@@ -2,5 +2,5 @@ document.getElementById('blog-btn').addEventListener('click', function(event){
     event.preventDefault();
 
     document.getElementById('toggle-btn').classList.add('hidden');
-    window.location = 'blog.html'
+    window.location = '/blog.html'
 })
--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 document.getElementById('home-btn').addEventListener('click', function(event){
     event.preventDefault();
 
-    window.location = 'index.html'
+    window.location = '/'
 })
