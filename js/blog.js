7fbf1de5ed80cfbf50a2ecfbb62ff36f2309976a
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-09-23 17:32:37 +0600

    blog hidden remoce

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,6 @@
 document.getElementById('blog-btn').addEventListener('click', function(event){
     event.preventDefault();
 
-    document.getElementById('toggle-btn').classList.add('hidden');
+    // document.getElementById('toggle-btn').classList.add('hidden');
     window.location = 'blog.html'
 })
